aa4fe509f9021df9eb01b9059212b04ad8b984d7
henriqueIFPE <hlf@discente.ifpe.edu.br>
2022-01-10 17:53:46 -0300

    altenticação

---
+++ src/main/java/com/ifpe/PP2/util/ServiceException.java
@@ -0,0 +1,14 @@
+package com.ifpe.PP2.util;
+
+public class ServiceException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ServiceException(String message) {
+		super(message);
+	}
+
+}
